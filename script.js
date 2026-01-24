f7fffe6395f20d54421bc44bfe632fd002e44c7f
Blerand Gashi <blerandgashi15@gmail.com>
2026-01-24 02:56:53 +0100

    first js steps of this project

--- script.js
+++ script.js
@@ -2,3 +2,21 @@ const inputEl = document.querySelector("#text-input");
 const notesContent = document.querySelector("#notes");
 
 const addBtn = document.querySelector("#add");
+
+
+addBtn.addEventListener("click", function(){
+  let inputText = inputEl.value;
+
+  const upperCaseFirstChar = inputText.charAt(0).toUpperCase() + inputText.slice(1);
+
+  let html = `
+    <div class="notes">
+      <p class="input-text-value">${upperCaseFirstChar}</p>
+      <button class="delete-btn">Delete</button>
+    </div>
+  `
+
+  notesContent.innerHTML += html;
+
+  inputEl.value = "";
+})
