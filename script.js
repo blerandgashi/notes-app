53449d4d53f6a3616524e99921d2f9f2ccbce6d3
Blerand Gashi <blerandgashi15@gmail.com>
2026-01-25 04:21:46 +0100

    added new features

--- script.js
+++ script.js
@@ -3,20 +3,38 @@ const notesContent = document.querySelector("#notes");
 
 const addBtn = document.querySelector("#add");
 
+function renderNotes(){
+  const notes = JSON.parse(localStorage.getItem("notes")) || [];
+
+  notesContent.innerHTML = "";
+
+  notes.forEach(notesEl => {
+    let html = `
+      <div class="notes">
+        <p class="input-text-value">${notesEl}</p>
+        <button class="delete-btn">Delete</button>
+      </div>
+    `
+    notesContent.innerHTML += html;
+    
+  });
+}
 
 addBtn.addEventListener("click", function(){
+
   let inputText = inputEl.value;
+  
+  if (!inputText) {
+    return
+  }
 
   const upperCaseFirstChar = inputText.charAt(0).toUpperCase() + inputText.slice(1);
-
-  let html = `
-    <div class="notes">
-      <p class="input-text-value">${upperCaseFirstChar}</p>
-      <button class="delete-btn">Delete</button>
-    </div>
-  `
-
-  notesContent.innerHTML += html;
-
+  
+  let notes = JSON.parse(localStorage.getItem("notes")) || [];
+  notes.push(upperCaseFirstChar);
+  localStorage.setItem("notes", JSON.stringify(notes));
+  
+  renderNotes()
   inputEl.value = "";
 })
+renderNotes()
